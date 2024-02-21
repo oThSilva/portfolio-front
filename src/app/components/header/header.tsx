5d15acde3346eee43b5248711e9203827afe3355
Thiago <thipereira.silva@hotmail.com>
2024-02-21 17:38:35 -0300

    feat: add initial html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "./header.scss"
+
+export function Header() {
+    return (
+        <div className="header">
+            <div>
+                <h1>Oi, eu sou o Thigo</h1>
+                <h2>Desenvolvedor Full Stack</h2>
+                <Image
+                    src="/me.jpg"
+                    alt="Minha foto"
+                    width={325}
+                    height={315}
+                    priority
+                />
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
-import Image from "next/image";
+import { Header } from "./components/header/header";
 
+import "./styles/home.scss"
 
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className="container">
+      <Header />
+      <div className="experiencia">
+        <h3>Experiência </h3>
+        <p>Sou o Thiago, tenho 23 anos, estudo desenvolvimento de software back end e front end. Estou em
+          busca da minha primeira experiência profissional na área de tecnologia.
+          Atualmente desenvolvo projetos com Django utilizando API/REST com os métodos HTTP GET e
+          POST, integrando com banco de dados relacional disponibilizado pela framework. Também
+          desenvolvo projetos responsivos com JavaScript, HTML e CSS.</p>
+        <div className="tempo-experiencia">
+
+        </div>
+        <div className="infos">
+          <h3>Linguagens</h3>
+          <div className="infos-linguagens">
+            <span>PT-BR - Língua Nativa </span>
+            <span>EN - Básico</span>
+          </div>
+          <h3>Educação</h3>
+          <div className="infos-educacao">
+            <span>Desenvolvimento Back End – Unopar
+            </span>
+            <span></span>
+          </div>
+
+        </div>
+        <div className="buttons">
+          <div className="social">
+          </div>
+          <button>Contate - me</button>
+        </div>
+      </div>
     </main >
   );
 }
